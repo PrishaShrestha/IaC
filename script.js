8173d69dde7573a576902e837063c3958aed874d
Prisha Shrestha <prishashsrestha03@gmail.com>
2026-08-11 10:49:52 +0545

    html,css,js completed

---
+++ script.js
@@ -0,0 +1,24 @@
+const screen = document.getElementById('screen');
+
+function appendValue(value) {
+  screen.value += value;
+}
+
+function clearScreen() {
+  screen.value = '';
+}
+
+function deleteLast() {
+  screen.value = screen.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    if (screen.value.trim() !== '') {
+      screen.value = eval(screen.value);
+    }
+  } catch (error) {
+    screen.value = 'Error';
+    setTimeout(clearScreen, 1500);
+  }
+}
